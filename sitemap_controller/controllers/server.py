d0ce2425d106a9c1f3ddf07cacf19c8409177860
suraw00t <surawut.kong@gmail.com>
2023-06-13 10:58:18 +0700

    refactor: :bulb: update schedule

--- sitemap_controller/controllers/server.py
+++ sitemap_controller/controllers/server.py
@@ -8,6 +8,60 @@ from sitemap_controller.crawler import Crawler
 logger = logging.getLogger(__name__)
 
 
+def find_next_date_from_weekday(weekday=None):
+    if weekday is None or weekday not in range(0, 7):
+        return
+    today = datetime.datetime.today()
+    if today.weekday() == weekday:
+        return today
+    elif today.weekday() < weekday:
+        time_delta = datetime.timedelta(days=weekday - today.weekday())
+        return today + time_delta
+    else:
+        time_delta = datetime.timedelta(days=7 - weekday - today.weekday())
+        return today + time_delta
+
+
+def datetime_schedule(schedule=None):
+    hours, minutes, dom, months, dow = schedule.strip().split(" ")
+    today = datetime.datetime.today()
+    hours = int(hours) if hours != "*" else 0
+    minutes = int(minutes) if minutes != "*" else 0
+    day = int(dom) if dom != "*" else today.day
+    month = int(months) if months != "*" else today.month
+    dow = int(dow) if dow != "*" else None
+    year = today.year
+
+    time_today = today.time().replace(second=0, microsecond=0)
+    time = datetime.time(hours, minutes, 0, 0)
+    date = datetime.date(today.year, month if month != today.month else month, day)
+    current_datetime = datetime.datetime.combine(date, time)
+
+    if months != "*" and dom == "*":
+        day = 1
+
+    if time < time_today and months == "*" and dom == "*":
+        day = day + 1
+
+    if current_datetime < today and dom != "*" and months == "*":
+        month = month + 1
+
+    if current_datetime < today and months != "*":
+        year = year + 1
+
+    if dow is not None:
+        day_of_week = find_next_date_from_weekday(dow)
+        if day_of_week.day == today.day and time < time_today:
+            day = day_of_week.day + 7
+        else:
+            day = day_of_week.day
+
+    date_time = datetime.datetime(year, month, day, hours, minutes, 0, 0)
+    schedule = abs(datetime.datetime.today() - date_time)
+    logger.info(f"next task at {date_time} : remaining {schedule}")
+    return schedule
+
+
 class ControllerServer:
     def __init__(self, settings):
         self.settings = settings
@@ -15,45 +69,14 @@ class ControllerServer:
 
         self.sitemap = SitemapController(self.settings)
 
-    def time_schedule(self, schedule=None):
-        hours, minutes, dom, months, dow = schedule.split(" ")
-        today = datetime.datetime.today()
-        hours = int(hours) if hours != "*" else 0
-        minutes = int(minutes) if minutes != "*" else 0
-        day = int(dom) if dom != "*" else today.day
-        month = int(months) if months != "*" else today.month
-        dow = int(dow) if dow != "*" else None
-        year = today.year
-
-        time_today = today.time().replace(second=0, microsecond=0)
-        time = datetime.time(hours, minutes, 0, 0)
-        date = datetime.date(today.year, month if month != today.month else month, day)
-        current_datetime = datetime.datetime.combine(date, time)
-        if current_datetime < today and months != "*":
-            year = year + 1
-
-        if current_datetime < today and dom != "*":
-            month = month + 1
-
-        if time < time_today:
-            day = day + 1
-
-        if dow is not None:
-            print("days of week", dow)
-
-        date_time = datetime.datetime(year, month, day, hours, minutes, 0, 0)
-        schedule = abs(datetime.datetime.today() - date_time)
-        logger.info(f"next task at {date_time} : remaining {schedule}")
-        return schedule
-
     async def controller_schedule(self):
         logger.debug("start controller schedule")
         time_check_schedule = self.settings.get("SCHEDULE_TO_CHECK_SITEMAP")
-        # await self.sitemap.generate_sitemap()
+        await self.sitemap.generate_sitemap()
 
         while self.running:
             logger.debug("running sitemap controller")
-            schedule = self.time_schedule(time_check_schedule)
+            schedule = datetime_schedule(time_check_schedule)
             await asyncio.sleep(schedule.total_seconds())
             await self.sitemap.generate_sitemap()
 
