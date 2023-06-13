c878f53ed9c1cd91ce75416a5b49ffcf578e97b1
suraw00t <surawut.kong@gmail.com>
2023-06-13 13:38:15 +0700

    refactor: :bulb: add run function

--- run-controller.py
+++ run-controller.py
@@ -1,4 +1,8 @@
-from sitemap_controller.cmd import controller
+def run_controller():
+    from sitemap_controller.cmd import controller
+
+    controller.main()
+
 
 if __name__ == "__main__":
-    controller.main()
+    run_controller()
