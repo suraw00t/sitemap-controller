80d0af295bd05990f183aa01c32b1e4a927200ee
suraw00t <surawut.kong@gmail.com>
2023-07-04 15:26:40 +0700

    refactor: 💡 update default settings

--- sitemap_controller/default_settings.py
+++ sitemap_controller/default_settings.py
@@ -1,18 +1,10 @@
+BASE_URL = "https://www.legardy.com/"
+
+DATABASE_CLIENT = "postgresql"
+DATABASE_HOST = "postgresdb"
+DATABASE_PORT = 5432
+DATABASE_NAME = "legardy-strapi-db"
+DATABASE_USERNAME = "admin"
+DATABASE_PASSWORD = "adminpw"
+
 SCHEDULE_TO_CHECK_SITEMAP = "0 0 1 * *"
-URL = "http://localhost/"
-HTTP_REQUEST_SSL_OPTION = True
-EXCLUDE_URLS = [
-    ".pdf",
-    ".jpg",
-    ".zip",
-    ".css",
-    ".js",
-    ".png",
-    ".jpeg",
-    ".mp4",
-    ".mp3",
-    ".rar",
-    ".xz",
-    ".gz",
-    ".exe",
-]
